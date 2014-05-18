6a204bf5bab146a220400b6a5944e132af5e7f0e
Brian Hines <brian@projectweekend.net>
2014-05-18 13:07:05 -0500

    Add drop box lib

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 import os
+import dropbox
 from flask import Flask
 from flask import render_template
 
 
 app = Flask(__name__)
+
+DROPBOX_KEY = os.getenv('DROPBOX_KEY')
+DROPBOX_SECRET = os.getenv('DROPBOX_SECRET')
+
 
 
 @app.route('/')
