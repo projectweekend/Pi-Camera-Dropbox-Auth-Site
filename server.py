7655629f558bf461b8e31c97f799b4462268cd80
Brian Hines <brian@projectweekend.net>
2014-05-18 14:18:31 -0500

    Get token back

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import os
 from dropbox.client import DropboxOAuth2Flow, DropboxClient
 from flask import Flask
 from flask import session
+from flask import request
 from flask import render_template
 
 
@@ -28,4 +29,12 @@ def index():
 
 @app.route('/token')
 def function():
-    return render_template('token.html')
+    flow = DropboxOAuth2Flow(DROPBOX_KEY, DROPBOX_SECRET, REDIRECT_URI,
+                                session, 'dropbox-auth-csrf-token')
+    access_token, user_id, url_state = flow.finish(request.args)
+    template_data = {
+        'token': access_token,
+        'user_id': user_id,
+        'url_state': url_state
+    }
+    return render_template('token.html', **template_data)
